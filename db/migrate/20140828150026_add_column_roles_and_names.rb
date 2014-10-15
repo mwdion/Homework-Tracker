af6541b65cb7b34308bcee818b13edd718c2d9f2
Michael Dion <michaeldion@Michaels-MacBook-Pro.local>
2014-10-15 10:47:18 -0400

    fixed migration error with duplicate column names

--- db/migrate/20140828150026_add_column_roles_and_names.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnRolesAndNames < ActiveRecord::Migration
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :role, :string
-  end
-end
